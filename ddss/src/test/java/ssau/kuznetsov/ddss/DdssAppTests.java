39c92f4529c2d895c45c509058eb12646c5aa53b
Andrey <ethenaut@gmail.com>
2022-04-27 14:54:47 +0400

    create template project

---
+++ ddss/src/test/java/ssau/kuznetsov/ddss/DdssAppTests.java
@@ -0,0 +1,13 @@
+package ssau.kuznetsov.ddss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdssAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

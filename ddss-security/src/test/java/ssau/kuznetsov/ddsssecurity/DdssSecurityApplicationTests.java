252468028dbf0251f33ec57d7a24ab9e2fcdab8e
Andrey <ethenaut@gmail.com>
2022-04-28 13:49:55 +0400

    create DDSS security package

---
+++ ddss-security/src/test/java/ssau/kuznetsov/ddsssecurity/DdssSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.kuznetsov.ddsssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdssSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

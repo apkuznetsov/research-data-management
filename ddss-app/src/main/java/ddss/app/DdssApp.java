9787134102f64ef2f26a789d803e459321eb00a2
Andrey Kuznetsov <ethenaut@ya.ru>
2022-04-29 19:48:33 +0400

    create DDSS app template module

---
+++ ddss-app/src/main/java/ddss/app/DdssApp.java
@@ -0,0 +1,13 @@
+package ddss.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdssApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdssApp.class, args);
+    }
+
+}

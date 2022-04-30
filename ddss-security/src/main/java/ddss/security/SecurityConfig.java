00175c5d68b0b8990e64d6b59a839b6867412a77
Andrey Kuznetsov <ethenaut@ya.ru>
2022-04-30 09:01:28 +0400

    refactor security config

--- ddss-security/src/main/java/ddss/security/SecurityConfig.java
+++ ddss-security/src/main/java/ddss/security/SecurityConfig.java
@@ -7,19 +7,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder encoder() {
-        return new StandardPasswordEncoder("53cr3t");
+//        return new StandardPasswordEncoder("53cr3t");
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override

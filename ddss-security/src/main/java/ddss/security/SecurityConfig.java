58853b66601aa9bf6becf3536960abb58ddbb10e
Andrey Kuznetsov <ethenaut@ya.ru>
2022-04-30 15:05:18 +0400

    config security

--- ddss-security/src/main/java/ddss/security/SecurityConfig.java
+++ ddss-security/src/main/java/ddss/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,5 +32,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+
+                .antMatchers("/cat/register", "/cat/login")
+                .permitAll()
+
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .httpBasic();
     }
 }

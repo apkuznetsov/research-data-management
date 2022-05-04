16243c30eb05c4c9d5fbbad4b77b4de328358e1c
Andrey Kuznetsov <ethenaut@ya.ru>
2022-05-04 20:28:42 +0400

    create test class

---
+++ ddss-catalog/src/test/java/ddss/app/DdssCatalogAppTests.java
@@ -0,0 +1,13 @@
+package ddss.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdssCatalogAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

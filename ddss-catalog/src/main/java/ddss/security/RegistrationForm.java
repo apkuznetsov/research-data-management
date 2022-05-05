bd7c91235ca5b8360242a93c597240a2223b4430
Andrey Kuznetsov <ethenaut@ya.ru>
2022-05-05 22:15:18 +0400

    add registration form constructors

--- ddss-catalog/src/main/java/ddss/security/RegistrationForm.java
+++ ddss-catalog/src/main/java/ddss/security/RegistrationForm.java
@@ -11,6 +11,15 @@ public class RegistrationForm {
     private String password;
     private String about;
 
+    public RegistrationForm() {
+    }
+
+    public RegistrationForm(String username, String password, String about) {
+        this.username = username;
+        this.password = password;
+        this.about = about;
+    }
+
     public DeviceUser toDeviceUser(PasswordEncoder passwordEncoder) {
         return new DeviceUser(
                 username, passwordEncoder.encode(password),

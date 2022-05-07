f492fc3ea7088f6644aecd964267758590d0fd4c
Andrey <ethenaut@gmail.com>
2022-05-07 21:07:50 +0400

    test create record by id with status created

--- ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
+++ ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
@@ -5,18 +5,51 @@ import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.*;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CatalogIntegrationTests extends IntegrationTests {
 
     private static final String CAT_RECORD = "/cat/record";
+    private static final String CREATE = "create";
+
     private static final String TEST_USERNAME = "kuznetsov";
     private static final String TEST_PASSWORD = "qwerty";
+
     private static final int TEST_RECORD_ID = 11;
     private static final String TEST_RECORD_ABOUT = "test catalog record 1";
     private static final String TEST_RECORD_PROTO_SCHEME = "message SensorData { int32 data = 1; }";
+
+    private static final int TEST_RECORD_FOR_CREATE_ID = 13;
+    private static final String TEST_RECORD_FOR_CREATE_ABOUT = "test catalog record for create";
+    private static final String TEST_RECORD_FOR_CREATE_PROTO_SCHEME = "message SensorData { int32 data = 1; }";
+
     private static final int TEST_RECORD_ID_FOR_FORBIDDEN = 12;
     private static final int TEST_RECORD_ID_FOR_NOT_FOUND = 13;
+
+    @Test
+    @FlywayTest
+    public void create_record_by_id_with_status_created() {
+        // arrange
+        String testUrl = CAT_RECORD + "/" + CREATE;
+        CatalogRecord record = new CatalogRecord(
+                TEST_RECORD_FOR_CREATE_ID, TEST_RECORD_FOR_CREATE_ABOUT, TEST_RECORD_FOR_CREATE_PROTO_SCHEME,
+                LocalDateTime.now());
+        HttpEntity<CatalogRecord> request = new HttpEntity<>(record, new HttpHeaders());
+
+        // act
+        ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
+                .exchange(testUrl, HttpMethod.POST, request, CatalogRecord.class);
+        CatalogRecord result = response.getBody();
+
+        // assert
+        assert result != null;
+        assertEquals(TEST_RECORD_FOR_CREATE_ID, result.getId());
+        assertEquals(TEST_RECORD_FOR_CREATE_ABOUT, result.getAbout());
+        assertEquals(TEST_RECORD_FOR_CREATE_PROTO_SCHEME, result.getProtoScheme());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
 
     @Test
     @FlywayTest

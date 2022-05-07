678e88bc92182892f0ccdae2f98e1ca9abca72ec
Andrey <ethenaut@gmail.com>
2022-05-07 20:03:30 +0400

    test get record by id with status OK

--- ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
+++ ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
@@ -13,12 +13,14 @@ public class CatalogIntegrationTests extends IntegrationTests {
     private static final String TEST_USERNAME = "kuznetsov";
     private static final String TEST_PASSWORD = "qwerty";
     private static final int TEST_RECORD_ID = 11;
+    private static final String TEST_RECORD_ABOUT = "test catalog record 1";
+    private static final String TEST_RECORD_PROTO_SCHEME = "message SensorData { int32 data = 1; }";
     private static final int TEST_RECORD_ID_FOR_FORBIDDEN = 12;
     private static final int TEST_RECORD_ID_FOR_NOT_FOUND = 13;
 
     @Test
     @FlywayTest
-    public void get_record_by_id() {
+    public void get_record_by_id_with_status_ok() {
         // arrange
         String testUrl = CAT_RECORD + "/" + TEST_RECORD_ID;
         HttpEntity<String> request = new HttpEntity<>(null, new HttpHeaders());
@@ -29,6 +31,10 @@ public class CatalogIntegrationTests extends IntegrationTests {
         CatalogRecord result = response.getBody();
 
         // assert
+        assert result != null;
+        assertEquals(TEST_RECORD_ID, result.getId());
+        assertEquals(TEST_RECORD_ABOUT, result.getAbout());
+        assertEquals(TEST_RECORD_PROTO_SCHEME, result.getProtoScheme());
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
@@ -42,7 +48,6 @@ public class CatalogIntegrationTests extends IntegrationTests {
         // act
         ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
                 .exchange(testUrl, HttpMethod.DELETE, request, CatalogRecord.class);
-        CatalogRecord result = response.getBody();
 
         // assert
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
@@ -58,7 +63,6 @@ public class CatalogIntegrationTests extends IntegrationTests {
         // act
         ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
                 .exchange(testUrl, HttpMethod.DELETE, request, CatalogRecord.class);
-        CatalogRecord result = response.getBody();
 
         // assert
         assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
@@ -74,7 +78,6 @@ public class CatalogIntegrationTests extends IntegrationTests {
         // act
         ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
                 .exchange(testUrl, HttpMethod.DELETE, request, CatalogRecord.class);
-        CatalogRecord result = response.getBody();
 
         // assert
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());

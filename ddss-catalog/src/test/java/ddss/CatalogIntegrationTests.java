bce0e09300a206a40e5c872c505dd8098e9245d4
Andrey <ethenaut@gmail.com>
2022-05-07 19:40:05 +0400

    refactor catalog integration tests

--- ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
+++ ddss-catalog/src/test/java/ddss/CatalogIntegrationTests.java
@@ -5,23 +5,24 @@ import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.*;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CatalogIntegrationTests extends IntegrationTests {
 
     private static final String CAT_RECORD = "/cat/record";
+    private static final String TEST_USERNAME = "kuznetsov";
+    private static final String TEST_PASSWORD = "qwerty";
+    private static final int TEST_RECORD_1_ID = 11;
 
     @Test
     @FlywayTest
-    public void get_record_by_id() throws IOException {
+    public void get_record_by_id() {
         // arrange
-        String testUrl = CAT_RECORD + "/" + 11;
+        String testUrl = CAT_RECORD + "/" + TEST_RECORD_1_ID;
         HttpEntity<String> request = new HttpEntity<>(null, new HttpHeaders());
 
         // act
-        ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth("kuznetsov", "qwerty")
+        ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
                 .exchange(testUrl, HttpMethod.GET, request, CatalogRecord.class);
         CatalogRecord result = response.getBody();
 
@@ -31,13 +32,13 @@ public class CatalogIntegrationTests extends IntegrationTests {
 
     @Test
     @FlywayTest
-    public void delete_record_with_status_no_content() throws Exception {
+    public void delete_record_with_status_no_content() {
         // arrange
-        String testUrl = CAT_RECORD + "/" + 11;
+        String testUrl = CAT_RECORD + "/" + TEST_RECORD_1_ID;
         HttpEntity<String> request = new HttpEntity<>(null, new HttpHeaders());
 
         // act
-        ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth("kuznetsov", "qwerty")
+        ResponseEntity<CatalogRecord> response = restTemplate.withBasicAuth(TEST_USERNAME, TEST_PASSWORD)
                 .exchange(testUrl, HttpMethod.DELETE, request, CatalogRecord.class);
         CatalogRecord result = response.getBody();
 

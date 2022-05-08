339df8018646ceff8b10e2fedce301fd32e7fe90
Andrey <ethenaut@gmail.com>
2022-05-08 12:04:11 +0400

    create DDSS storage template

---
+++ ddss-storage/src/main/java/ddss/DdssStorageApp.java
@@ -0,0 +1,13 @@
+package ddss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdssStorageApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdssStorageApp.class, args);
+    }
+
+}

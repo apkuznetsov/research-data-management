fcad5a56c3430d178d7f9bf7634a7cce235e970d
Andrey <ethenaut@gmail.com>
2022-05-08 20:52:59 +0400

    create controller templates

---
+++ ddss-storage/src/main/java/ddss/api/DownloadController.java
@@ -0,0 +1,19 @@
+package ddss.api;
+
+import ddss.domain.DeviceUser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/storage")
+public class DownloadController {
+
+    @PostMapping(value = "/download", consumes = "application/json")
+    public ResponseEntity<String> download(@AuthenticationPrincipal DeviceUser user) {
+        return new ResponseEntity<>("data downloaded", HttpStatus.OK);
+    }
+}

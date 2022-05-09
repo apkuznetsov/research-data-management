30b23adefde281df6e8e0ec9f03d624b1bf398f1
Andrey <ethenaut@gmail.com>
2022-05-09 21:30:20 +0400

    save on DB uploaded data

--- ddss-storage/src/main/java/ddss/storage/api/UploadController.java
+++ ddss-storage/src/main/java/ddss/storage/api/UploadController.java
@@ -1,8 +1,11 @@
 package ddss.storage.api;
 
+import ddss.storage.data.DepositRepository;
 import ddss.storage.domain.Data;
+import ddss.storage.domain.Deposit;
 import ddss.storage.domain.DeviceUser;
 import ddss.storage.domain.Feedback;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,15 +14,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
 @RequestMapping("/storage")
 public class UploadController {
+
+    @Autowired
+    DepositRepository depositRepo;
 
     @PostMapping(value = "/upload", consumes = "application/json")
     public ResponseEntity<Feedback> upload(@RequestBody Data data, @AuthenticationPrincipal DeviceUser user) {
         Feedback feedback = new Feedback(
                 "bytes " + data.getBytes() + " with " + data.getBytes().length() + " length uploaded",
                 data.getBytes());
+
+        Deposit deposit = new Deposit(1, LocalDateTime.now(), data.getBytes().getBytes());
+        depositRepo.save(deposit);
+
         return new ResponseEntity<>(feedback, HttpStatus.OK);
     }
 }

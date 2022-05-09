829b378c795bd5e0aeafcadaf75508707ac27e9c
Andrey <ethenaut@gmail.com>
2022-05-09 21:30:42 +0400

    impl storage download

--- ddss-storage/src/main/java/ddss/storage/api/DownloadController.java
+++ ddss-storage/src/main/java/ddss/storage/api/DownloadController.java
@@ -1,19 +1,28 @@
 package ddss.storage.api;
 
+import ddss.storage.data.DepositRepository;
+import ddss.storage.domain.Data;
+import ddss.storage.domain.Deposit;
 import ddss.storage.domain.DeviceUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/storage")
 public class DownloadController {
 
-    @PostMapping(value = "/download", consumes = "application/json")
-    public ResponseEntity<String> download(@AuthenticationPrincipal DeviceUser user) {
-        return new ResponseEntity<>("data downloaded", HttpStatus.OK);
+    @Autowired
+    DepositRepository depositRepo;
+
+    @GetMapping(value = "/download", consumes = "application/json")
+    public ResponseEntity<Data> download(@AuthenticationPrincipal DeviceUser user) {
+        Deposit deposit = depositRepo.findDepositByCatalogRecordId(1);
+        Data data = new Data(deposit.getData());
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }

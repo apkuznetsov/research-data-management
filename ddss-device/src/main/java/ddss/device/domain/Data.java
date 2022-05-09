4d59e9d84e34a4bbc6fa2e96b67c099eb1fb9f7e
Andrey <ethenaut@gmail.com>
2022-05-09 18:04:15 +0400

    represent data bytes as a string

--- ddss-device/src/main/java/ddss/device/domain/Data.java
+++ ddss-device/src/main/java/ddss/device/domain/Data.java
@@ -3,16 +3,19 @@ package ddss.device.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 @Getter
 @Setter
 public class Data {
 
-    byte[] bytes;
+    String bytes;
 
     public Data() {
     }
 
     public Data(byte[] bytes) {
-        this.bytes = bytes;
+        this.bytes = new String(bytes, StandardCharsets.UTF_8);
     }
 }
--- ddss-storage/src/main/java/ddss/storage/domain/Data.java
+++ ddss-storage/src/main/java/ddss/storage/domain/Data.java
@@ -3,16 +3,18 @@ package ddss.storage.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.nio.charset.StandardCharsets;
+
 @Getter
 @Setter
 public class Data {
 
-    byte[] bytes;
+    String bytes;
 
     public Data() {
     }
 
     public Data(byte[] bytes) {
-        this.bytes = bytes;
+        this.bytes = new String(bytes, StandardCharsets.UTF_8);
     }
 }

c7753941842a01558269ee57404d56c2bc19aa45
Andrey <ethenaut@gmail.com>
2022-05-09 17:12:14 +0400

    impl feedback model

---
+++ ddss-device/src/main/java/ddss/device/domain/Feedback.java
@@ -0,0 +1,18 @@
+package ddss.device.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Feedback {
+
+    private String message;
+
+    public Feedback() {
+    }
+
+    public Feedback(String message) {
+        this.message = message;
+    }
+}

7b974de178ff5858b73f6e78bbf0c0d4ca7fc9f2
Andrey <ethenaut@gmail.com>
2022-05-10 21:33:35 +0400

    copy test props

---
+++ ddss-catalog/src/test/java/ddss/catalog/DdssCatalogTestProps.java
@@ -0,0 +1,62 @@
+package ddss.catalog;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@Configuration
+@PropertySource("classpath:test.properties")
+public class DdssCatalogTestProps {
+
+    @Value("${user.username}")
+    private String username;
+    @Value("${user.password}")
+    private String password;
+    @Value("${user.about}")
+    private String about;
+
+    @Value("${user2.username}")
+    private String username2;
+    @Value("${user2.password}")
+    private String password2;
+    @Value("${user2.about}")
+    private String about2;
+
+    @Value("${url.storage.register}")
+    private String urlRegister;
+    @Value("${url.storage.available_megabytes}")
+    private String urlAvailableMegabytes;
+    @Value("${url.storage.upload}")
+    private String urlUpload;
+    @Value("${url.storage.download}")
+    private String urlDownload;
+
+    @Value("${cat_rec_id.upload_sensors_data}")
+    private int catRecIdUploadSensorsData;
+    @Value("${cat_rec_id.upload_person}")
+    private int catRecIdUploadPerson;
+    @Value("${cat_rec_id.download_ok}")
+    private int catRecIdDownloadOk;
+    @Value("${cat_rec_id.download_not_found}")
+    private int catRecIdDownloadNotFound;
+
+    @Value("${sensors_data.degrees_celsius}")
+    private int sensDataDegrees;
+    @Value("${sensors_data.pascals}")
+    private int sensDataPascals;
+    @Value("${sensors_data.meters_per_second}")
+    private int sensDataMetersPerSecond;
+
+    @Value("${person.id}")
+    private int personId;
+    @Value("${person.name}")
+    private String personName;
+    @Value("${person.surname}")
+    private String personSurname;
+    @Value("${person.email}")
+    private String personEmail;
+}

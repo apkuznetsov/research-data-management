6bfbc422bf39c46a05850d9dac809c7ae9befbfd
Andrey <ethenaut@gmail.com>
2022-05-10 14:44:40 +0400

    get available megabytes

---
+++ ddss-storage/src/main/java/ddss/storage/api/CatalogInteractionController.java
@@ -0,0 +1,31 @@
+package ddss.storage.api;
+
+import ddss.storage.domain.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+
+@Controller
+@RequestMapping("/storage/admin")
+public class CatalogInteractionController {
+
+    private static long calcAvailableMegabytes() {
+        long availableBytes = 0;
+
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            availableBytes += root.getFreeSpace();
+        }
+
+        return (long) ((double) availableBytes / 1024.0);
+    }
+
+    @GetMapping(value = "/available-megabytes")
+    public ResponseEntity<Long> getAvailableMegabytes() {
+        return new ResponseEntity<>(calcAvailableMegabytes(), HttpStatus.OK);
+    }
+}

03cc84979a0ffb80d8b8fb61e4b52b055fa69785
Andrey <ethenaut@gmail.com>
2022-05-10 19:44:54 +0400

    def user props

---
+++ ddss-storage/src/test/java/ddss/storage/DdssStorageTestProps.java
@@ -0,0 +1,28 @@
+package ddss.storage;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@Configuration
+@PropertySource("classpath:test.properties")
+public class DdssStorageTestProps {
+
+    @Value("${user.username}")
+    private String username;
+    @Value("${user.password}")
+    private String password;
+    @Value("${user.about}")
+    private String about;
+
+    @Value("${user2.username}")
+    private String username2;
+    @Value("${user2.password}")
+    private String password2;
+    @Value("${user2.about}")
+    private String about2;
+}

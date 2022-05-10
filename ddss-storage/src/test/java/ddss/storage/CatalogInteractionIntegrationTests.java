45d8211d2a7c3e6a14d4c5a006153c27ea4b7ad2
Andrey <ethenaut@gmail.com>
2022-05-10 16:48:45 +0400

    test get available megabytes with status unauthorized

--- ddss-storage/src/test/java/ddss/storage/CatalogInteractionIntegrationTests.java
+++ ddss-storage/src/test/java/ddss/storage/CatalogInteractionIntegrationTests.java
@@ -17,6 +17,8 @@ public class CatalogInteractionIntegrationTests extends IntegrationTests {
     private static final String STORAGE_AVAILABLE_MEGABYTES = "/storage/admin/available-megabytes";
     private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PASSWORD = "qwerty";
+    private static final String USERNAME = "kuznetsov";
+    private static final String PASSWORD = "qwerty";
 
     @Autowired
     public TestRestTemplate restTemplate;
@@ -36,5 +38,19 @@ public class CatalogInteractionIntegrationTests extends IntegrationTests {
         // assert
         assertTrue(result >= 0);
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @FlywayTest
+    public void get_available_megabytes_with_status_unauthorized() {
+        // arrange
+        HttpEntity<AvailableMegabytesNumber> request = new HttpEntity<>(null, new HttpHeaders());
+
+        // act
+        ResponseEntity<AvailableMegabytesNumber> response = restTemplate.withBasicAuth(USERNAME, PASSWORD)
+                .exchange(STORAGE_AVAILABLE_MEGABYTES, HttpMethod.GET, request, AvailableMegabytesNumber.class);
+
+        // assert
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
 }

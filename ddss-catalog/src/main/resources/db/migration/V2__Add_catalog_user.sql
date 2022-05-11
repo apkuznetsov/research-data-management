6871617e70379ebe6c0632ea23013606dffb5afb
Andrey <ethenaut@gmail.com>
2022-05-11 15:30:07 +0400

    insert IP addresses and ports

--- ddss-catalog/src/main/resources/db/migration/V2__Add_catalog_user.sql
+++ ddss-catalog/src/main/resources/db/migration/V2__Add_catalog_user.sql
@@ -1,8 +1,11 @@
-INSERT INTO catalog_user (id, username, password, about)
-VALUES (101, 'admin', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'admin');
+INSERT INTO catalog_user (id, username, password, about, ip_address, port)
+VALUES (101, 'admin', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'admin',
+        '127.0.0.1', 2048);
 
-INSERT INTO catalog_user (id, username, password, about)
-VALUES (11, 'kuznetsov', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'test device 1');
+INSERT INTO catalog_user (id, username, password, about, ip_address, port)
+VALUES (11, 'kuznetsov', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'test device 1',
+        '127.0.0.1', 2048);
 
-INSERT INTO catalog_user (id, username, password, about)
-VALUES (12, 'ivanov', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'test device 2');
+INSERT INTO catalog_user (id, username, password, about, ip_address, port)
+VALUES (12, 'ivanov', '$2a$10$nhz1A23fITpU.9MuxBpQL.ox.7UrzGiN/2CytweQZ7BMHSxaJ4nVO', 'test device 2',
+        '127.0.0.1', 2048);

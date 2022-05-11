4e59e89d4f31b0a68c67f0e17e35b1b46284eca1
Andrey <ethenaut@gmail.com>
2022-05-11 15:43:41 +0400

    delete storage user entity

--- ddss-catalog/src/main/resources/db/migration/V1__Init_DB.sql
+++ ddss-catalog/src/main/resources/db/migration/V1__Init_DB.sql
@@ -17,17 +17,6 @@ CREATE TABLE IF NOT EXISTS catalog_record
     catalog_user_id INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS storage_user
-(
-    id                         SERIAL PRIMARY KEY,
-    username                   VARCHAR(64) NOT NULL,
-    password                   VARCHAR(64) NOT NULL,
-    about                      VARCHAR(2048),
-    ip_address                 VARCHAR(15) NOT NULL,
-    port                       SMALLINT,
-    available_megabytes_number BIGINT      NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS catalog_with_storage_record
 (
     id                SERIAL PRIMARY KEY,
@@ -38,9 +27,6 @@ CREATE TABLE IF NOT EXISTS catalog_with_storage_record
 
 alter table catalog_user
     add constraint uniq_catalog_user_id_username unique (username);
-
-alter table storage_user
-    add constraint uniq_storage_user_username unique (username);
 
 alter table catalog_record
     add constraint fk_catalog_record_catalog_user_id

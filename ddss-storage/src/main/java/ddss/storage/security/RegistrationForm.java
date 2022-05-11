f615316cb40ddb013621ca058575dc335f6b7068
Andrey <ethenaut@gmail.com>
2022-05-11 16:24:28 +0400

    add new columns to reg form

--- ddss-storage/src/main/java/ddss/storage/security/RegistrationForm.java
+++ ddss-storage/src/main/java/ddss/storage/security/RegistrationForm.java
@@ -10,19 +10,26 @@ public class RegistrationForm {
     private String username;
     private String password;
     private String about;
+    private String ipAddress;
+    private short port;
+    private boolean isStorage;
 
     public RegistrationForm() {
     }
 
-    public RegistrationForm(String username, String password, String about) {
+    public RegistrationForm(String username, String password, String about,
+                            String ipAddress, short port, boolean isStorage) {
         this.username = username;
         this.password = password;
         this.about = about;
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.isStorage = isStorage;
     }
 
     public CatalogUser toDeviceUser(PasswordEncoder passwordEncoder) {
         return new CatalogUser(
-                username, passwordEncoder.encode(password),
-                about);
+                username, passwordEncoder.encode(password), about,
+                ipAddress, port, isStorage);
     }
 }

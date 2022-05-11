98d8bffc0711e4dfeb3301aea25efdce43b71760
Andrey <ethenaut@gmail.com>
2022-05-11 17:42:00 +0400

    def catalog user constraints

--- ddss-storage/src/main/java/ddss/storage/domain/CatalogUser.java
+++ ddss-storage/src/main/java/ddss/storage/domain/CatalogUser.java
@@ -3,6 +3,7 @@ package ddss.storage.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,12 +24,16 @@ public class CatalogUser implements UserDetails {
     @Column(name = "id", nullable = false)
     private Integer id;
     @Column(name = "username", nullable = false)
+    @Length(min = 5, max = 64)
     private String username;
     @Column(name = "password", nullable = false)
+    @Length(min = 5, max = 64)
     private String password;
     @Column(name = "about")
+    @Length(max = 256)
     private String about;
     @Column(name = "ip_address", nullable = false)
+    @Length(max = 15)
     private String ipAddress;
     @Column(name = "port", nullable = false)
     private short port;

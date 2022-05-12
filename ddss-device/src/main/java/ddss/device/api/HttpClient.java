140211302ae60294ef4647d1f66d652cf7d49916
Andrey <ethenaut@gmail.com>
2022-05-12 09:09:31 +0400

    revert "use Test REST template instead of REST template"
    this reverts commit b25b814e4c90f5a3922f1283c6c3db996ff634a6.

--- ddss-device/src/main/java/ddss/device/api/HttpClient.java
+++ ddss-device/src/main/java/ddss/device/api/HttpClient.java
@@ -1,12 +1,26 @@
 package ddss.device.api;
 
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HttpClient {
 
-    public static final TestRestTemplate httpClient;
+    public static final RestTemplate httpClient;
 
     static {
-        httpClient = new TestRestTemplate();
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(
+                Collections.singletonList(MediaType.APPLICATION_JSON));
+        messageConverters.add(converter);
+
+        httpClient = new RestTemplate();
+        httpClient.setMessageConverters(messageConverters);
     }
 }

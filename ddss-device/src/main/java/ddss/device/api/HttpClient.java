b25b814e4c90f5a3922f1283c6c3db996ff634a6
Andrey <ethenaut@gmail.com>
2022-05-12 09:09:31 +0400

    use Test REST template instead of REST template

--- ddss-device/src/main/java/ddss/device/api/HttpClient.java
+++ ddss-device/src/main/java/ddss/device/api/HttpClient.java
@@ -1,26 +1,12 @@
 package ddss.device.api;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 public class HttpClient {
 
-    public static final RestTemplate httpClient;
+    public static final TestRestTemplate httpClient;
 
     static {
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(
-                Collections.singletonList(MediaType.APPLICATION_JSON));
-        messageConverters.add(converter);
-
-        httpClient = new RestTemplate();
-        httpClient.setMessageConverters(messageConverters);
+        httpClient = new TestRestTemplate();
     }
 }

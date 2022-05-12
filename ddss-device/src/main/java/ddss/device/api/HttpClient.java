479f1a8dbc16e7dff84c1d10283dcc6bc22f20b7
Andrey <ethenaut@gmail.com>
2022-05-12 11:23:06 +0400

    add Jackson Datatype JSR310 to HTTP client

--- ddss-device/src/main/java/ddss/device/api/HttpClient.java
+++ ddss-device/src/main/java/ddss/device/api/HttpClient.java
@@ -1,9 +1,12 @@
 package ddss.device.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,10 +20,14 @@ public class HttpClient {
     public static final RestTemplate httpClient;
 
     static {
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .modules(new JavaTimeModule());
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(builder.build());
         converter.setSupportedMediaTypes(
                 Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
         messageConverters.add(converter);
 
         httpClient = new RestTemplate();

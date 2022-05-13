96dff13b146a3d6d106e3042a24ef0b5814da92e
Andrey <ethenaut@gmail.com>
2022-05-13 09:30:06 +0400

    def catalog storage model

---
+++ ddss-device/src/main/java/ddss/device/domain/CatalogStorage.java
@@ -0,0 +1,21 @@
+package ddss.device.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CatalogStorage {
+
+    private int id;
+    private String about;
+    private String ipAddress;
+    private short port;
+
+    public CatalogStorage(int id, String about, String ipAddress, short port) {
+        this.id = id;
+        this.about = about;
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+}

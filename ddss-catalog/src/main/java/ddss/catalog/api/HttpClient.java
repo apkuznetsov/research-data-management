4bdf29aafcddfde1747816d1d63dc1ddefc546f6
Andrey <ethenaut@gmail.com>
2022-05-14 11:04:43 +0400

    fix create headers

--- ddss-catalog/src/main/java/ddss/catalog/api/HttpClient.java
+++ ddss-catalog/src/main/java/ddss/catalog/api/HttpClient.java
@@ -1,22 +1,22 @@
 package ddss.catalog.api;
 
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class HttpClient {
 
     public static RestTemplate client = new RestTemplate();
 
     public static HttpHeaders createHeaders(String username, String password) {
-        return new HttpHeaders() {{
-            String auth = username + ":" + password;
-            byte[] encodedAuth = Base64.encodeBase64(
-                    auth.getBytes(StandardCharsets.US_ASCII));
-            String authHeader = "Basic " + new String(encodedAuth);
-            set("Authorization", authHeader);
-        }};
+        String notEncoded = username + ":" + password;
+        String encodedAuth = "Basic " + Base64.getEncoder().encodeToString(notEncoded.getBytes(StandardCharsets.UTF_8));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", encodedAuth);
+        return headers;
     }
 }

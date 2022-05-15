a40e9e785152dff06fb603ac5e1fb0415e41e7e0
Andrey <ethenaut@gmail.com>
2022-05-15 10:39:58 +0400

    create test sensors data

--- ddss-device/src/main/java/ddss/device/api/StorageInteractionController.java
+++ ddss-device/src/main/java/ddss/device/api/StorageInteractionController.java
@@ -1,4 +1,13 @@
 package ddss.device.api;
 
+import ddss.device.proto.SensorsData;
+
 public class StorageInteractionController {
+
+    private static final SensorsData protoSensData = SensorsData.newBuilder()
+            .setDegreesCelsius(44)
+            .setPascals(12)
+            .setMetersPerSecond(13).build();
+    private static final Data dataSensData = new Data(protoSensData.toByteArray());
+
 }

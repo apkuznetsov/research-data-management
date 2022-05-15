1e397b655a1a30dddcb6b8ade93f7aedd9b6769b
Andrey <ethenaut@gmail.com>
2022-05-15 09:25:39 +0400

    find catalog record by id

--- ddss-catalog/src/main/java/ddss/catalog/data/CatalogRecordRepository.java
+++ ddss-catalog/src/main/java/ddss/catalog/data/CatalogRecordRepository.java
@@ -4,6 +4,11 @@ import ddss.catalog.domain.CatalogRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CatalogRecordRepository extends JpaRepository<CatalogRecord, Integer> {
+
+    Optional<CatalogRecord> findById(Integer id);
+
 }

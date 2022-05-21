c04e6b0501e6ba32c85f8244a835b2bee25546b0
Andrey <ethenaut@gmail.com>
2022-05-21 17:41:44 +0400

    default simulation constants

--- ddss-device/src/main/java/ddss/device/simulation/DeviceSimulation.java
+++ ddss-device/src/main/java/ddss/device/simulation/DeviceSimulation.java
@@ -6,10 +6,13 @@ import ddss.device.proto.SensorsData;
 
 public class DeviceSimulation {
 
+    private static final int DEFAULT_DEGREES_CELSIUS = 44;
+    private static final int DEFAULT_PASCALS = 12;
+    private static final int DEFAULT_METERS_PER_SECOND = 13;
     public static final SensorsData PROTO_SENSORS_DATA = SensorsData.newBuilder()
-            .setDegreesCelsius(44)
-            .setPascals(12)
-            .setMetersPerSecond(13).build();
+            .setDegreesCelsius(DEFAULT_DEGREES_CELSIUS)
+            .setPascals(DEFAULT_PASCALS)
+            .setMetersPerSecond(DEFAULT_METERS_PER_SECOND).build();
     public static final Data DATA = new Data(PROTO_SENSORS_DATA.toByteArray());
 
     public static String toString(Data data) {

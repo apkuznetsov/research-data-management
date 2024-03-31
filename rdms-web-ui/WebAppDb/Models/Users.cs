3de62db7d922b7713454e870a6e45f8121d82376
Андрей Кузнецов <ethenaut@ya.ru>
2024-03-31 22:01:14 +0400

    def users

---
+++ rdms-web-ui/WebAppDb/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WebappDb
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            UserExperiments = new HashSet<UserExperiments>();
+        }
+
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Surname { get; set; }
+        public string Forename { get; set; }
+
+        public virtual ICollection<UserExperiments> UserExperiments { get; set; }
+    }
+}

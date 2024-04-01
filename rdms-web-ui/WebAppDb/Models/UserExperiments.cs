fbef68750f9cbac7625b4268cb61b78050c06add
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-01 20:13:10 +0400

    def user experiments

---
+++ rdms-web-ui/WebAppDb/Models/UserExperiments.cs
@@ -0,0 +1,12 @@
+﻿namespace WebappDb
+{
+    public partial class UserExperiments
+    {
+        public int UserExperimentsId { get; set; }
+        public int UserId { get; set; }
+        public int ExperimentId { get; set; }
+
+        public virtual Experiments Experiment { get; set; }
+        public virtual Users User { get; set; }
+    }
+}

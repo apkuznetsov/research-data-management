d41865d551a54a0f79b2c8d225a3b5d6b2c32b4f
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-02 18:42:34 +0400

    def test storage files

---
+++ rdms-web-ui/WebAppDb/Models/TestStorageFiles.cs
@@ -0,0 +1,13 @@
+﻿namespace WebappDb
+{
+    public partial class TestStorageFiles
+    {
+        public int TestStorageFileId { get; set; }
+        public int StorageFileId { get; set; }
+        public int TestId { get; set; }
+        public bool IsInput { get; set; }
+
+        public virtual StorageFiles StorageFile { get; set; }
+        public virtual Tests Test { get; set; }
+    }
+}

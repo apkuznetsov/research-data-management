e206078a44c25caf0e8e0265d887e9029afe1763
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-04 22:32:23 +0400

    def test params

---
+++ rdms-web-ui/WebAppDb/Models/TestParams.cs
@@ -0,0 +1,12 @@
+﻿namespace WebappDb
+{
+    public partial class TestParams
+    {
+        public int TestParamId { get; set; }
+        public int TestId { get; set; }
+        public int MetadataParameterId { get; set; }
+
+        public virtual MetadataParameters MetadataParameter { get; set; }
+        public virtual Tests Test { get; set; }
+    }
+}

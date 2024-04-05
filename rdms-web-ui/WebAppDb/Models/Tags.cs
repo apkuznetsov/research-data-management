fa4fdf4ae12cacc75cfe68fdb4774778f92b5270
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-05 20:58:10 +0400

    def tags

---
+++ rdms-web-ui/WebAppDb/Models/Tags.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WebappDb
+{
+    public partial class Tags
+    {
+        public Tags()
+        {
+            ExperimentTags = new HashSet<ExperimentTags>();
+        }
+
+        public int TagId { get; set; }
+        public string Value { get; set; }
+
+        public virtual ICollection<ExperimentTags> ExperimentTags { get; set; }
+    }
+}

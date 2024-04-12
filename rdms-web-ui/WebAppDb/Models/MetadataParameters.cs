4fa37ed95aac90e0c21036fccee5eb081b5283bb
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-12 22:23:19 +0400

    def metadata parameters

---
+++ rdms-web-ui/WebAppDb/Models/MetadataParameters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WebappDb
+{
+    public partial class MetadataParameters
+    {
+        public MetadataParameters()
+        {
+            ExperimentParams = new HashSet<ExperimentParams>();
+            TestParams = new HashSet<TestParams>();
+        }
+
+        public int MetadataParameterId { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public virtual ICollection<ExperimentParams> ExperimentParams { get; set; }
+        public virtual ICollection<TestParams> TestParams { get; set; }
+    }
+}

5acc022ec091bf7970ecc00cb293a3fc564e9f9a
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-13 23:34:17 +0400

    def experiment tags

---
+++ rdms-web-ui/WebAppDb/Models/ExperimentTags.cs
@@ -0,0 +1,12 @@
+﻿namespace WebappDb
+{
+    public partial class ExperimentTags
+    {
+        public int ExperimentTagId { get; set; }
+        public int ExperimentId { get; set; }
+        public int TagId { get; set; }
+
+        public virtual Experiments Experiment { get; set; }
+        public virtual Tags Tag { get; set; }
+    }
+}

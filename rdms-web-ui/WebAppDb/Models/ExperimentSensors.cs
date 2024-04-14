99d28e511e1458528c555866256c43a01712e4a2
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-14 22:59:23 +0400

    def experiment sensors

---
+++ rdms-web-ui/WebAppDb/Models/ExperimentSensors.cs
@@ -0,0 +1,12 @@
+﻿namespace WebappDb
+{
+    public partial class ExperimentSensors
+    {
+        public int ExperimentSensorId { get; set; }
+        public int ExperimentId { get; set; }
+        public int SensorId { get; set; }
+
+        public virtual Experiments Experiment { get; set; }
+        public virtual Sensors Sensor { get; set; }
+    }
+}

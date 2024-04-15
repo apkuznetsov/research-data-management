f8ce8cfe0a333c48f80980983efd32294d65ae71
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-15 19:18:24 +0400

    def experiments

---
+++ rdms-web-ui/WebAppDb/Models/Experiments.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebappDb
+{
+    public partial class Experiments
+    {
+        public Experiments()
+        {
+            ExperimentParams = new HashSet<ExperimentParams>();
+            ExperimentSensors = new HashSet<ExperimentSensors>();
+            ExperimentTags = new HashSet<ExperimentTags>();
+            Tests = new HashSet<Tests>();
+            UserExperiments = new HashSet<UserExperiments>();
+        }
+
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "Дата создания")]
+        public DateTime CreatedAt { get; set; }
+
+        public virtual ICollection<ExperimentParams> ExperimentParams { get; set; }
+        public virtual ICollection<ExperimentTags> ExperimentTags { get; set; }
+        public virtual ICollection<ExperimentSensors> ExperimentSensors { get; set; }
+        public virtual ICollection<Tests> Tests { get; set; }
+        public virtual ICollection<UserExperiments> UserExperiments { get; set; }
+    }
+}

2350d7822ea4bbd01272a7e7ec15d63cd5a2462f
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-16 23:38:15 +0400

    def experiment params

---
+++ rdms-web-ui/WebAppDb/Models/ExperimentParams.cs
@@ -0,0 +1,12 @@
+﻿namespace WebappDb
+{
+    public partial class ExperimentParams
+    {
+        public int ExperimentParamId { get; set; }
+        public int ExperimentId { get; set; }
+        public int MetadataParameterId { get; set; }
+
+        public virtual Experiments Experiment { get; set; }
+        public virtual MetadataParameters MetadataParameter { get; set; }
+    }
+}

a414bf74156edea136b5b47c601bb411abd6365e
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-17 22:09:17 +0400

    def datatypes

---
+++ rdms-web-ui/WebAppDb/Models/Datatypes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace WebappDb
+{
+    public partial class Datatypes
+    {
+        public Datatypes()
+        {
+            Sensors = new HashSet<Sensors>();
+        }
+
+        public int DataTypeId { get; set; }
+        public string Metadata { get; set; }
+        public string Schema { get; set; }
+
+        public virtual ICollection<Sensors> Sensors { get; set; }
+    }
+}

135ac89300468d78221f6a8c59ab858ac6e6497d
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-18 21:28:20 +0400

    def communication protocols

---
+++ rdms-web-ui/WebAppDb/Models/CommunicationProtocols.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WebappDb
+{
+    public partial class CommunicationProtocols
+    {
+        public CommunicationProtocols()
+        {
+            Sensors = new HashSet<Sensors>();
+        }
+
+        public int CommunicationProtocolId { get; set; }
+        public string ProtocolName { get; set; }
+
+        public virtual ICollection<Sensors> Sensors { get; set; }
+    }
+}

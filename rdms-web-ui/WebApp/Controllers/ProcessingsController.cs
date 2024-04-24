702372a83ff64c2bbb65addbca437f6078c3f323
Андрей Кузнецов <ethenaut@ya.ru>
2024-04-24 22:40:12 +0400

    processings controller

---
+++ rdms-web-ui/WebApp/Controllers/ProcessingsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Webapp.Models;
+using WebappDb;
+
+namespace Webapp.Controllers
+{
+    public class ProcessingsController : Controller
+    {
+        private readonly WebappdbContext db;
+
+        public ProcessingsController(WebappdbContext context)
+        {
+            db = context;
+        }
+
+        // GET: Processings
+        public IActionResult Index()
+        {
+            var processings = db.Processings.Select(m => new ProcessingNameViewModel { ProcessingId = m.ProcessingId, Name = m.Name }).ToList();
+
+            if (processings == null)
+            {
+                return NotFound();
+            }
+
+            return View(processings);
+        }
+    }
+}

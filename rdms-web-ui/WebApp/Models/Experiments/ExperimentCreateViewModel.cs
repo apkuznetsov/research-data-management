422155f53d49448e94d0a3ee9168403bc37154be
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-02 21:19:48 +0400

    create exp view model

---
+++ rdms-web-ui/WebApp/Models/Experiments/ExperimentCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models.Experiments
+{
+    public class ExperimentCreateViewModel
+    {
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Название"),
+            Required(ErrorMessage = "Введите название эксперимента")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание"),
+            Required(ErrorMessage = "Введите описание эксперимента")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "Датчик"),
+            Required(ErrorMessage = "Выберите датчик")]
+        public int SensorId { get; set; }
+
+    }
+}

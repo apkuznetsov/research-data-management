a1bf35cdd57d4da0fdc2c846c82b1092c0e00a18
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-03 18:33:12 +0400

    del exp view model

---
+++ rdms-web-ui/WebApp/Models/Experiments/ExperimentDeleteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models.Experiments
+{
+    public class ExperimentDeleteViewModel
+    {
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "Дата создания")]
+        public DateTime CreatedAt { get; set; }
+
+        public int ExperimentSensorId { get; set; }
+
+        [Display(Name = "Датчик")]
+        public string ExperimentSensorName { get; set; }
+    }
+}

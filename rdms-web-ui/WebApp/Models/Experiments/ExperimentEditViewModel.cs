c265970dec9c555c4e4d262beea06bfc189fcbef
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-05 22:26:10 +0400

    exp edit view model

---
+++ rdms-web-ui/WebApp/Models/Experiments/ExperimentEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models.Experiments
+{
+    public class ExperimentEditViewModel
+    {
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        public int ExperimentSensorId { get; set; }
+
+        [Display(Name = "Датчик")]
+        public string ExperimentSensorName { get; set; }
+    }
+}

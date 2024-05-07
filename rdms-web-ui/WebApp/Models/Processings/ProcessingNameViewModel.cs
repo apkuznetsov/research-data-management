39aa944f3f478835723c34cbc52cf5c20a670848
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-07 19:52:16 +0400

    processing name view model

---
+++ rdms-web-ui/WebApp/Models/Processings/ProcessingNameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models
+{
+    public class ProcessingNameViewModel
+    {
+        public int ProcessingId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+    }
+}

dd109410ad0bf1445bb8b65fa05864dc9c033eed
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-08 21:20:11 +0400

    create sensor view model

---
+++ rdms-web-ui/WebApp/Models/Sensors/SensorCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models
+{
+    public class SensorCreateViewModel
+    {
+        public int SensorId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "JSON-схема фиксируемых данных")]
+        public string DataType { get; set; }
+
+        [Display(Name = "Сетевой протокол")]
+        public int CommunicationProtocolId { get; set; }
+
+        [Display(Name = "IP-адрес")]
+        public string IpAddress { get; set; }
+
+        [Display(Name = "Порт")]
+        public int Port { get; set; }
+    }
+}

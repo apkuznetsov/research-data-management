1e95ecd1a4feaa3b018c401692bd34caf266afbf
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-03-16 16:47:58 +0400

    upload sensor models

---
+++ rdms-web-ui/WebApp/Models/Sensors/SensorEditViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models
+{
+    public class SensorEditViewModel
+    {
+        public int SensorId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        public int DataTypeId { get; set; }
+
+        [Display(Name = "JSON-схема фиксируемых данных")]
+        public string DatatypeScheme { get; set; }
+
+        [Display(Name = "Сетевой протокол")]
+        public int CommunicationProtocolId { get; set; }
+
+        [Display(Name = "IP-адрес")]
+        public string IpAddress { get; set; }
+
+        [Display(Name = "Порт")]
+        public int Port { get; set; }
+    }
+}
---
+++ rdms-web-ui/WebApp/Models/Sensors/SensorNameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models
+{
+    public class SensorNameViewModel
+    {
+        public int SensorId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+    }
+}

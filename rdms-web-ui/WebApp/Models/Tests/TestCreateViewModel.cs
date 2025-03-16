2cb36d74393f195ff06fe9dbf542e23d9d5a4fd5
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-03-16 16:47:46 +0400

    upload test models

---
+++ rdms-web-ui/WebApp/Models/Tests/TestCreateViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models.Tests
+{
+    public class TestCreateViewModel
+    {
+        public int TestId { get; set; }
+
+        [Display(Name = "Название"),
+            Required(ErrorMessage = "Введите название измерения")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание"),
+            Required(ErrorMessage = "Введите описание измерения")]
+        public string Metadata { get; set; }
+
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Эксперимент")]
+        public string ExperimentName { get; set; }
+
+        public int ExperimentSensorId { get; set; }
+
+        [Display(Name = "Датчик")]
+        public string ExperimentSensorName { get; set; }
+
+        [Display(Name = "Длительность измерения в секундах"),
+            Required(ErrorMessage = "Введите длительность"),
+            Range(1, 3600, ErrorMessage = "Максимальная длительность – 3600 секунд")]
+        public int DurationSeconds { get; set; }
+
+        [Display(Name = "Время начала")]
+        public DateTime StartedTime { get; set; }
+
+        [Display(Name = "Время завершения")]
+        public DateTime? EndedTime { get; set; }
+    }
+}
---
+++ rdms-web-ui/WebApp/Models/Tests/TestDetailsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webapp.Models.Tests
+{
+    public class TestDetailsViewModel
+    {
+        public int TestId { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "Время начала")]
+        public DateTime StartedTime { get; set; }
+
+        [Display(Name = "Время завершения")]
+        public DateTime? EndedTime { get; set; }
+
+        public int ExperimentId { get; set; }
+
+        [Display(Name = "Эксперимент")]
+        public string ExperimentName { get; set; }
+
+        public int ExperimentSensorId { get; set; }
+
+        [Display(Name = "Датчик")]
+        public string ExperimentSensorName { get; set; }
+    }
+}
